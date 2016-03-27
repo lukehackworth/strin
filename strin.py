ff98d7b0b4fd81e3bc0af85d780f53e7bd1b5c0e
luke <luke@hackstation.hackdomain>
2016-03-27 09:04:04 -0500

    Create a functional genetic algorithm

--- strin.py
+++ strin.py
@@ -28,14 +28,54 @@ def main():
         machine_string_array = mutate_array_strings(
             machine_string_array, letter_flip_probability
         )
+        
+        output = []
+        for mch_str in machine_string_array:
+            #mch_str_num_array = string_to_num_array(mch_str)
+            output.append(string_to_num_array(mch_str))
+        
+        output2 = []
+        for num_list in output:
+            j = dist_find(num_list, input_num_array)
+            output2.append(j)
+            
+        output3 = []
+        for num_list in output2:
+            j = array_average(num_list)
+            output3.append(j)
+        
+        m = min(output3)
+        for idx, val in enumerate(output3):
+            if val == m:
+                machine_string = machine_string_array[idx]
+        print(str(machine_string_array))
+        print(machine_string)
+        #average_dist_array = create_dist_averaged_array(machine_string_array)
+        
+        #averaged_num_array = create_array_average(machine_string_array, input_array_average)
 
-        averaged_num_array = create_array_average(machine_string_array)
+        #print(machine_string_array)
+        #print(averaged_num_array)
 
-        print(machine_string_array)
-        print(averaged_num_array)
+        #machine_string = machine_string_array[find_best_string(
+        #    averaged_num_array, input_array_average)]
 
-        machine_string = machine_string_array[find_best_string(
-            averaged_num_array, input_array_average)]
+def dist_find(array1, array2):
+    # given two arrays of numbers, find average of both arrays for each 
+    # thing in list.
+    # eg 1 = [a, b]; 2 = [b, c]; output = [abs(a-b), abs(b-c)]
+    output = []
+    for idx, val in enumerate(array1):
+        m = abs(val - array2[idx])
+        output.append(m)
+    return(output)
+
+def create_dist_averaged_array(array_to_average, input_array_average):
+    output_array = []
+    for num in array_to_average:
+        dist = abs(num - input_array_average)
+        output_array.append(dist)
+    return output_array
 
 
 def find_best_string(tmp_array, input_array_average):
